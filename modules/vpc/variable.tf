41f0eff11ee513b66bb4885282b9a0178cf81176
Basavaraju <mrbasavaraju123@gmail.com>
2024-05-08 14:58:06 +0530

    vpc modules-changes

--- modules/vpc/variable.tf
+++ modules/vpc/variable.tf
@@ -8,12 +8,8 @@ variable "subnet_count" {
   default     = 3
 }
 
-variable "subnet_cidr" {
-  description = "CIDR blocks for subnets"
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-}
-
-variable "availability_zones" {
-  description = "List of availability zones"
-  default     = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+variable "subnet_cidrs" {
+  type = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  
 }
